3e4405c9b49dc2e9e2d28495acab7164fe7260b4
RaphaelStn <raphaelstacino@gmail.com>
2022-04-30 10:20:15 +0200

    adding voxel model

--- pages/index.js
+++ pages/index.js
@@ -1,83 +1,88 @@
 import { Container, Box, Heading, Image, useColorModeValue, Button, Link } from "@chakra-ui/react"
 import { ChevronRightIcon } from "@chakra-ui/icons"
 import NextLink from 'next/link'
+import Layout from "../components/layouts/article"
 import Section from "../components/section"
 import Paragraph from "../components/parapraph"
 import { BioSection, BioYear } from "../components/Bio"
-import Head from "next/head"
 
 
 const Page = () => {
     return (
-        <Container>
-            <Box borderRadius="lg" bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')} p={3} mb={6} align="center">
-                Hello I&apos;m a full-stack developer based in France.
-            </Box>
-            <Box display={{md:"flex"}}>
-                <Box flexGrow={1}>
-                    <Heading as="h2" variant="page-title">
-                        Raphaël Stacino
+        <Layout>
+            <Container>
+                <Box borderRadius="lg" bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')} p={3} mb={6} align="center">
+                    Hello I&apos;m a full-stack developer based in France.
+                </Box>
+                <Box display={{md:"flex"}}>
+                    <Box flexGrow={1}>
+                        <Heading as="h2" variant="page-title">
+                            Raphaël Stacino
+                        </Heading>
+                    </Box>
+                    <Box 
+                    flexShrink={0} 
+                    mt={{base:4, md:0}}
+                    ml={{md:6}}
+                    align="center"
+                    >
+                        <Image 
+                        borderColor="whiteAlpha.800" 
+                        borderWidth={2}
+                        borderRadius="full"
+                        borderStyle="solid"
+                        maxWidth="100px"
+                        display="inline-block"
+                        src="/images/raphael.jpg"
+                        alt="profile picture"/>
+                    </Box>
+                </Box>
+                <Section delay= {0.1}>
+                    <Heading as="h3" variant="section-title">
+                        Work
                     </Heading>
-                </Box>
-                <Box 
-                flexShrink={0} 
-                mt={{base:4, md:0}}
-                ml={{md:6}}
-                align="center"
-                >
-                    <Image 
-                    borderColor="whiteAlpha.800" 
-                    borderWidth={2}
-                    borderRadius="full"
-                    borderStyle="solid"
-                    maxWidth="100px"
-                    display="inline-block"
-                    src="/images/raphael.jpg"
-                    alt="profile picture"/>
-                </Box>
-            </Box>
-            <Section delay= {0.1}>
-                <Heading as="h3" variant="section-title">
-                    Work
-                </Heading>
-                <Paragraph>
-                    Raphaël is a french full-stack developer based near Paris. You can visit my portfolio to see my work, every source code is available free to use on my&nbsp;
-                    <NextLink href="https://github.com/RaphaelStn" passHref><Link target="_blank">Github</Link></NextLink>. Working front-end with Javascript, React or Next, HTML and Sass, and back-end with PHP, MySql, NodeJS, Express, MongoDB.
-                </Paragraph>
-                <Box align="center" my={4}>
-                    <NextLink href="/works">
-                        <Button rightIcon={<ChevronRightIcon/>} colorScheme="blue">
-                            My Portfolio
-                        </Button>
-                    </NextLink>
-                </Box>
-            </Section>
-            <Section delay={0.2}>
-                <Heading as="h3" variant="section-title">
-                    Bio
-                </Heading>
-                <BioSection><BioYear>1994</BioYear>Born in Enghiens-les-bains, France.</BioSection>
-                <BioSection><BioYear>2013</BioYear>Obtained a Baccalauréat Scientifique.</BioSection>
-                <BioSection><BioYear>2015</BioYear>Butcher 🥩 in various shops in Le Havre and Paris</BioSection>
-                <BioSection><BioYear>2022</BioYear>Web Developer diploma obtained thanks to&nbsp; 
-                    <NextLink href="https://openclassrooms.com/" passHref >
-                        <Link target="_blank">
-                            OpenClassroom
-                        </Link>
-                    </NextLink>
-                    .
-                </BioSection>
-                <BioSection><BioYear>2022</BioYear>Independant Web Developer</BioSection>
-            </Section>
-            <Section delay={0.3}>
-                <Heading as="h3" variant="section-title">
-                    I ♥ 
-                </Heading>
-                <Paragraph>
-                    Art, Music, Aquariums & Plants
-                </Paragraph>
-            </Section>
-        </Container>
+                    <Paragraph>
+                        Raphaël is a french full-stack developer based near Paris. 
+                        You can visit my portfolio to see my work, every source code is available free to use on my&nbsp;
+                        <NextLink href="https://github.com/RaphaelStn" passHref><Link target="_blank">Github</Link></NextLink>
+                        . 
+                        Working front-end with Javascript, React or Next, HTML and Sass, and back-end with PHP, MySql, NodeJS, Express, MongoDB.
+                    </Paragraph>
+                    <Box align="center" my={4}>
+                        <NextLink href="/works">
+                            <Button rightIcon={<ChevronRightIcon/>} colorScheme="blue">
+                                My Portfolio
+                            </Button>
+                        </NextLink>
+                    </Box>
+                </Section>
+                <Section delay={0.2}>
+                    <Heading as="h3" variant="section-title">
+                        Bio
+                    </Heading>
+                    <BioSection><BioYear>1994</BioYear>Born in Enghiens-les-bains, France.</BioSection>
+                    <BioSection><BioYear>2013</BioYear>Obtained a Baccalauréat Scientifique.</BioSection>
+                    <BioSection><BioYear>2015</BioYear>Butcher 🥩 in various shops in Le Havre and Paris</BioSection>
+                    <BioSection><BioYear>2022</BioYear>Web Developer diploma obtained thanks to&nbsp; 
+                        <NextLink href="https://openclassrooms.com/" passHref >
+                            <Link target="_blank">
+                                OpenClassroom
+                            </Link>
+                        </NextLink>
+                        .
+                    </BioSection>
+                    <BioSection><BioYear>2022</BioYear>Independant Web Developer</BioSection>
+                </Section>
+                <Section delay={0.3}>
+                    <Heading as="h3" variant="section-title">
+                        I ♥ 
+                    </Heading>
+                    <Paragraph>
+                        Art, Music, Aquariums & Plants
+                    </Paragraph>
+                </Section>
+            </Container>
+        </Layout>
     )
 }
 
--- pages/works.js
+++ pages/works.js
@@ -1,5 +1,6 @@
 import { Container, Heading, SimpleGrid, Divider, useColorModeValue } from '@chakra-ui/react' 
 import Section from '../components/section'
+import Layout from "../components/layouts/article"
 import { GridItem, GridItemStyle } from '../components/grid-item'
 import whattodo from '../public/images/siteshot/whattodo.jpeg'
 import ireki from '../public/images/siteshot/wordpress.jpeg'
@@ -9,48 +10,50 @@ import spawnloop from '../public/images/siteshot/spawnloop.jpeg'
 
 const Works = () => {
     return (
-        <Container>
-            <Heading as="h3" fontSize={20} mb={4}>
-                Works
-            </Heading>
-
-            <SimpleGrid columns={[1,1,2]} gap={6}>
-                <Section>
-                    <GridItem href="https://whattodo.raphael-stacino.fr/?" title="WhatToDo" thumbnail={whattodo}> 
-                        Simple website to help see and choose between popular recent movies/shows. Written in PHP/Twig for back-end, and HTML/Sass/JS for the front-end.
-                    </GridItem>
-                </Section>
-                <Section>
-                    <GridItem href="https://ireki.raphael-stacino.fr/?" title="Ireki" thumbnail={ireki}> 
-                        Wordpress based website.
-                    </GridItem>
-                </Section>
-                <Section delay={0.1}>
-                    <GridItem href="https://webagency.raphael-stacino.fr/?" title="WebAgency" thumbnail={webagency}> 
-                        Template website for html/css work, my first project.
-                    </GridItem>
-                </Section>
-            </SimpleGrid>
-            
-            <Section delay={0.2}>
-                <Divider my={6} bg={useColorModeValue('grey', '')}/>
+        <Layout>
+            <Container>
                 <Heading as="h3" fontSize={20} mb={4}>
-                    Personnal Projects
+                    Works
                 </Heading>
-            </Section>
-            <SimpleGrid columns={[1,1,2]} gap={6}>  
+
+                <SimpleGrid columns={[1,1,2]} gap={6}>
+                    <Section>
+                        <GridItem href="https://whattodo.raphael-stacino.fr/?" title="WhatToDo" thumbnail={whattodo}> 
+                            Simple website to help see and choose between popular recent movies/shows. Written in PHP/Twig for back-end, and HTML/Sass/JS for the front-end.
+                        </GridItem>
+                    </Section>
+                    <Section>
+                        <GridItem href="https://ireki.raphael-stacino.fr/?" title="Ireki" thumbnail={ireki}> 
+                            Wordpress based website.
+                        </GridItem>
+                    </Section>
+                    <Section delay={0.1}>
+                        <GridItem href="https://webagency.raphael-stacino.fr/?" title="WebAgency" thumbnail={webagency}> 
+                            Template website for html/css work, my first project.
+                        </GridItem>
+                    </Section>
+                </SimpleGrid>
+                
                 <Section delay={0.2}>
-                    <GridItem href="https://heroesloop.raphael-stacino.fr/?" title="HeroesLoop" thumbnail={heroesloop}> 
-                        A Javascript demo for basic movement with keyboard, collision system, and gravity.
-                    </GridItem>
+                    <Divider my={6} bg={useColorModeValue('grey', '')}/>
+                    <Heading as="h3" fontSize={20} mb={4}>
+                        Personnal Projects
+                    </Heading>
                 </Section>
-                <Section delay={0.2}>
-                    <GridItem href="https://spawnloop.raphael-stacino.fr/?" title="SpawnLoop" thumbnail={spawnloop}> 
-                        A simple game with Javascript.
-                    </GridItem>
-                </Section>
-            </SimpleGrid>
-        </Container>
+                <SimpleGrid columns={[1,1,2]} gap={6}>  
+                    <Section delay={0.2}>
+                        <GridItem href="https://heroesloop.raphael-stacino.fr/?" title="HeroesLoop" thumbnail={heroesloop}> 
+                            A Javascript demo for basic movement with keyboard, collision system, and gravity.
+                        </GridItem>
+                    </Section>
+                    <Section delay={0.2}>
+                        <GridItem href="https://spawnloop.raphael-stacino.fr/?" title="SpawnLoop" thumbnail={spawnloop}> 
+                            A simple game with Javascript.
+                        </GridItem>
+                    </Section>
+                </SimpleGrid>
+            </Container>
+        </Layout>
     )
 }
 
