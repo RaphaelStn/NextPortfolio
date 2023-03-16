6c000f6d2819644186d2ae0035ef725e4fe6512e
RaphaelStn <raphaelstacino@gmail.com>
2023-03-16 12:24:23 +0100

    flex direction fix

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -7,7 +7,7 @@ import Footer from '../footer'
 
 const Main = ({children, router}) => {
     return (
-        <Box as="main" pb={8}  minHeight="100vh">
+        <Box as="main" pb={8}  minHeight="100vh" display="flex" flexDirection="column">
             <Head>
                 <meta name="viewport" content="width-device-width, initial-scale=1"/>
                 <title>Raphaël Stacino</title>
